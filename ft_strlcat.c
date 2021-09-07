29f1e913d48501078b95f5d0cce5a90c634ee3ae
Tomas Turner <tturner@F2N013.42adel.org.au>
2021-09-07 10:16:39 +0930

    backup

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -4,20 +4,15 @@ unsigned int	ft_strlcat(char *dest, const char *src, size_t size)
 {
 	unsigned int	i;
 	unsigned int	j;
+	int destlen;
+	int srclen;
 
 	i = 0;
 	j = 0;
-	while (dest[i] != '\0')
-		i++;
-	while (src[j] != '\0' && j < size - 1)
-	{
-		dest[i + j] = src[j];
-		j++;
-	}
-	if (size != 0)
-		dest[i + j] = '\0';
-	j = 0;
-	while (src[j] != '\0')
-		j++;
-	return (i + j);
+	destlen = ft_strlen(dest);
+	srclen = ft_strlen(src);
+
+	if (destlen >= size)
+		destlen = size;
+	return (destlen + srclen);
 }
