657d8903465baef38b7aaf37314eff79ca5972f5
Tomas Turner <tturner@F2N013.42adel.org.au>
2021-09-07 18:28:52 +0930

    split doesnt work

--- Makefile
+++ Makefile
@@ -25,7 +25,8 @@ SRCS = ft_atoi.c \
 			ft_strdup.c \
 			ft_substr.c \
 			ft_strjoin.c \
-			ft_strtrim.c
+			ft_strtrim.c \
+			ft_split.c \
 
 OBJS = $(SRCS:.c=.o)
 
--- ft_split.c
+++ ft_split.c
@@ -1,3 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tturner <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/07 16:54:31 by tturner           #+#    #+#             */
+/*   Updated: 2021/09/07 16:54:31 by tturner          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-char	**ft_split(char const *s, char c);
+static int	countsplits(char const *s, char c)
+{
+	int	count;
+	int len;
+	int i;
+
+	len = ft_strlen(s);
+	count = 0;
+	i = 0;
+	while (i < len)
+	{
+		if (s[i] == c)
+			count++;
+			while (s[i] != '\0' && s[i] == c)
+				i++;
+		i++;
+	}
+	return (count);
+}
+
