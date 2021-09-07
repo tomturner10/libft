e622e5ea0720ab599050a1eb2cc1364e2448f156
Tomas Turner <tturner@F2N013.42adel.org.au>
2021-09-07 11:53:31 +0930

    strlcat working

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,18 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tturner <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/07 10:44:13 by tturner           #+#    #+#             */
+/*   Updated: 2021/09/07 10:44:13 by tturner          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-unsigned int	ft_strlcat(char *dest, const char *src, size_t size)
+unsigned int	ft_strlcat(char *dest, const char *src, size_t dstsize)
 {
-	unsigned int	i;
-	unsigned int	j;
-	int destlen;
-	int srclen;
+	size_t	j;
+	size_t	destlen;
+	size_t	srclen;
 
-	i = 0;
 	j = 0;
 	destlen = ft_strlen(dest);
 	srclen = ft_strlen(src);
-
-	if (destlen >= size)
-		destlen = size;
+	while (j + destlen < dstsize - 1 && src[j] != '\0' && dstsize != 0)
+	{
+		dest[destlen + j] = src[j];
+		j++;
+	}
+	dest[destlen + j] = '\0';
+	if (destlen >= dstsize)
+		destlen = dstsize;
 	return (destlen + srclen);
 }
